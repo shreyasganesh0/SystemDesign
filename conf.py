00f4d64d94a373aeff739480f28f1336313bfe1b
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-10-29 20:31:14 -0400

    Add the FastAPI application server

--- conf.py
+++ conf.py
@@ -4,4 +4,4 @@ class Settings(BaseSettings):
     app_name: str = "Server Application"
     deafult_port: str = "8000"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra='allow')
--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import os
 from functools import lru_cache
 from typing_extensions import Annotated
 from uvicorn import Config, Server
-from . import conf
+from conf import Settings
 
 app = FastAPI()
 
 @lru_cache
 def get_settings():
-    return conf.Settings()
+    return Settings()
 
 @app.get("/")
-async def root(settings: Annotated[conf.Settings, Depends(get_settings)]):
+async def root(settings: Annotated[Settings, Depends(get_settings)]):
     port = os.environ.get("PORT", settings.port_number)
     return f'Hello {port}'
